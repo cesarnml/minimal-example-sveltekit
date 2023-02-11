d111b8725487accee9b0ec395f3008cca8a18729
Cesar Mejia <cmejia@gmail.com>
2023-02-11 11:26:56 +0700

    format

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,32 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'airbnb-base',
+		'prettier'
+	],
 	plugins: ['svelte3', '@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	overrides: [
+		{
+			files: ['*.svelte'],
+			processor: 'svelte3/svelte3',
+			rules: {
+				'import/no-extraneous-dependencies': 0,
+				'import/extensions': 0,
+				'import/newline-after-import': 0,
+				'import/first': 0,
+				'import/no-duplicates': 0,
+				'import/no-mutable-exports': 0,
+				'import/no-unresolved': 0,
+				'import/prefer-default-export': 0,
+				'import/order': 0,
+				'@typescript-eslint/no-use-before-define': 0
+			}
+		}
+	],
 	settings: {
 		'svelte3/typescript': () => require('typescript')
 	},
@@ -17,4 +39,4 @@ module.exports = {
 		es2017: true,
 		node: true
 	}
-};
+}
